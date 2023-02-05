23763ef213943ae6652b9084a59c15b4864ef1e9
jhi <julianhirtenfelder@gmail.com>
2023-02-05 15:56:16 +0100

    Initial commit

---
+++ src/test/java/org/campus02/hirtenfelder/BMICalculatorTest.java
@@ -0,0 +1,27 @@
+package org.campus02.hirtenfelder;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+    private BMICalculator bmiCalculator;
+
+    @BeforeEach
+    void setUp() {
+        this.bmiCalculator = new BMICalculator(180, 1.8);
+    }
+
+    @Test
+    void testConstructor() {
+        double expectedWeight = 180;
+        Assertions.assertEquals(expectedWeight, bmiCalculator.getWeightInKG());
+        Assertions.assertEquals(1.8, bmiCalculator.getHeightInMeters());
+    }
+
+    @Test
+    void result() {
+    }
+}
